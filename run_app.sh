43aa761bfee29a74d237e596e9fab165dda4a89c
ermishechkin <erman15@yandex.ru>
2017-04-26 18:51:07 +0300

    Add gunicorn restart (2)

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,2 @@
 docker build -t ask .
-docker run -d -p 80:80 -e MYSQL_HOST=172.17.0.2 --name ask ask python manage.py runserver 0.0.0.0:80
+docker run -d -p 80:80 -e MYSQL_HOST=172.17.0.2 --name ask ask
