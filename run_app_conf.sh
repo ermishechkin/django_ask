b1fb702c062fd4f99eab11ff9de12834144dbc66
ermishechkin <erman15@yandex.ru>
2017-04-26 18:32:06 +0300

    Port expose

--- run_app_conf.sh
+++ run_app_conf.sh
@@ -1,2 +1,2 @@
 docker build -t ask .
-docker run -it --rm -e MYSQL_HOST=172.17.0.2 --name ask ask bash
+docker run -p 80:80 -it --rm -e MYSQL_HOST=172.17.0.2 --name ask ask bash
--- run_haproxy.sh
+++ run_haproxy.sh
@@ -1,1 +1,1 @@
-docker run -d --name hap -v "$(pwd)/hap_etc:/usr/local/etc/haproxy:ro" haproxy:1.7
+docker run -d -p 80:80 --name hap -v "$(pwd)/hap_etc:/usr/local/etc/haproxy:ro" haproxy:1.7
